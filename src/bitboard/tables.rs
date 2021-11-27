8f9a815973c386a84261cdce812172ef704f84b6
Jetpuffed <brandon@jetpuffed.org>
2021-11-27 16:05:40 -0700

    Create empty generator functions and empty lookup tables

--- src/bitboard/tables.rs
+++ src/bitboard/tables.rs
@@ -1,1 +1,16 @@
 //! <Module description goes here>
+
+use super::Bitboard;
+
+pub const KNIGHT_ATTACK_LUT: [Bitboard; 0] = [];
+pub const BISHOP_ATTACK_LUT: [Bitboard; 0] = [];
+pub const ROOK_ATTACK_LUT: [Bitboard; 0] = [];
+pub const KING_ATTACK_LUT: [Bitboard; 0] = [];
+
+pub fn create_knight_attack_lut() { todo!() }
+
+pub fn create_bishop_attack_lut() { todo!() }
+
+pub fn create_rook_attack_lut() { todo!() }
+
+pub fn create_king_attack_lut() { todo!() }
