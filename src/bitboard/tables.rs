0677f4c9ee81ff8632760110cb70ca5d14fd4f33
Jetpuffed <brandon@jetpuffed.org>
2021-11-27 16:14:05 -0700

    Create more empty generator functions and lookup tables

--- src/bitboard/tables.rs
+++ src/bitboard/tables.rs
@@ -7,6 +7,11 @@ pub const BISHOP_ATTACK_LUT: [Bitboard; 0] = [];
 pub const ROOK_ATTACK_LUT: [Bitboard; 0] = [];
 pub const KING_ATTACK_LUT: [Bitboard; 0] = [];
 
+pub const KNIGHT_MASK_LUT: [Bitboard; 0] = [];
+pub const BISHOP_MASK_LUT: [Bitboard; 0] = [];
+pub const ROOK_MASK_LUT: [Bitboard; 0] = [];
+pub const KING_MASK_LUT: [Bitboard; 0] = [];
+
 pub fn create_knight_attack_lut() { todo!() }
 
 pub fn create_bishop_attack_lut() { todo!() }
@@ -14,3 +19,11 @@ pub fn create_bishop_attack_lut() { todo!() }
 pub fn create_rook_attack_lut() { todo!() }
 
 pub fn create_king_attack_lut() { todo!() }
+
+pub fn create_knight_mask_lut() { todo!() }
+
+pub fn create_bishop_mask_lut() { todo!() }
+
+pub fn create_rook_mask_lut() { todo!() }
+
+pub fn create_king_mask_lut() { todo!() }
