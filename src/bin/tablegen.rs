fedef89b3bb782565ae3340d579a917b1b9e63a6
Jetpuffed <brandon@jetpuffed.org>
2021-11-28 13:00:24 -0700

    Modify tablegen to output tables with multiple values on one line

--- src/bin/tablegen.rs
+++ src/bin/tablegen.rs
@@ -21,37 +21,86 @@ fn main() -> Result<(), Error>
         .write(true)
         .open("tables.txt")?;
 
-    // Create a new buffer to hold all formatted tables before writing to file
+    // Create a new buffer to hold all formatted tables before writing to file.
     let mut buf = String::new();
+
+    // Create a closure expression to fit multiple values on a single line.
+    let compress_table = |lut|
+    {
+        let mut output = Vec::new();
+        let mut fmt_buf = Vec::new();
+
+        for bb in lut
+        {
+            fmt_buf.push(format!("{:#018X}", bb));
+
+            if fmt_buf.len() == 8
+            {
+                output.push(fmt_buf.join(", ") + ",");
+                fmt_buf.clear();
+            }
+        }
+
+        if !fmt_buf.is_empty() { output.push(fmt_buf.join(", ")) }
+
+        output
+    };
 
     /*
         Mask lookup table generators
     */
 
-    buf.push_str(format!("pub const KNIGHT_MASK_LUT: [Bitboard; 64] = {:#018X?};\n", create_knight_mask_lut()).as_str());
-    buf.push_str(format!("pub const BISHOP_MASK_LUT: [Bitboard; 64] = {:#018X?};\n", create_bishop_mask_lut()).as_str());
-    buf.push_str(format!("pub const ROOK_MASK_LUT: [Bitboard; 64] = {:#018X?};\n", create_rook_mask_lut()).as_str());
-    buf.push_str(format!("pub const KING_MASK_LUT: [Bitboard; 64] = {:#018X?};\n", create_king_mask_lut()).as_str());
+    let lut = compress_table(create_knight_mask_lut().to_vec());
+    buf.push_str("pub const KNIGHT_MASK_LUT: [Bitboard; 64] = [\n");
+    for s in lut { buf.push_str(&("    ".to_owned() + &s + "\n")) }
+    buf.push_str("];\n\n");
+
+    let lut = compress_table(create_bishop_mask_lut().to_vec());
+    buf.push_str("pub const BISHOP_MASK_LUT: [Bitboard; 64] = [\n");
+    for s in lut { buf.push_str(&("    ".to_owned() + &s + "\n")) }
+    buf.push_str("];\n\n");
+
+    let lut = compress_table(create_rook_mask_lut().to_vec());
+    buf.push_str("pub const ROOK_MASK_LUT: [Bitboard; 64] = [\n");
+    for s in lut { buf.push_str(&("    ".to_owned() + &s + "\n")) }
+    buf.push_str("];\n\n");
+
+    let lut = compress_table(create_king_mask_lut().to_vec());
+    buf.push_str("pub const KING_MASK_LUT: [Bitboard; 64] = [\n");
+    for s in lut { buf.push_str(&("    ".to_owned() + &s + "\n")) }
+    buf.push_str("];\n\n");
 
     /*
         Attack lookup table generators
     */
 
     let (lut, offset) = create_knight_attack_lut();
-    buf.push_str(format!("pub const KNIGHT_ATTACK_LUT: [Bitboard; {}] = {:#018X?};\n", lut.len(), lut).as_str());
-    buf.push_str(format!("pub const KNIGHT_ATTACK_OFFSET: [Bitboard; 64] = {:?};\n", offset).as_str());
+    buf.push_str(format!("pub const KNIGHT_ATTACK_LUT: [Bitboard; {}] = [\n", lut.len()).as_str());
+    for s in compress_table(lut) { buf.push_str(&("    ".to_owned() + &s + "\n")) }
+    buf.push_str("];\n\n");
+
+    buf.push_str(format!("pub const KNIGHT_ATTACK_OFFSET: [Bitboard; 64] = {:?};\n\n", offset).as_str());
 
     let (lut, offset) = create_bishop_attack_lut();
-    buf.push_str(format!("pub const BISHOP_ATTACK_LUT: [Bitboard; {}] = {:#018X?};\n", lut.len(), lut).as_str());
-    buf.push_str(format!("pub const BISHOP_ATTACK_OFFSET: [Bitboard; 64] = {:?};\n", offset).as_str());
+    buf.push_str(format!("pub const BISHOP_ATTACK_LUT: [Bitboard; {}] = [\n", lut.len()).as_str());
+    for s in compress_table(lut) { buf.push_str(&("    ".to_owned() + &s + "\n")) }
+    buf.push_str("];\n\n");
+
+    buf.push_str(format!("pub const BISHOP_ATTACK_OFFSET: [Bitboard; 64] = {:?};\n\n", offset).as_str());
 
     let (lut, offset) = create_rook_attack_lut();
-    buf.push_str(format!("pub const ROOK_ATTACK_LUT: [Bitboard; {}] = {:#018X?};\n", lut.len(), lut).as_str());
-    buf.push_str(format!("pub const ROOK_ATTACK_OFFSET: [Bitboard; 64] = {:?};\n", offset).as_str());
+    buf.push_str(format!("pub const ROOK_ATTACK_LUT: [Bitboard; {}] = [\n", lut.len()).as_str());
+    for s in compress_table(lut) { buf.push_str(&("    ".to_owned() + &s + "\n")) }
+    buf.push_str("];\n\n");
+
+    buf.push_str(format!("pub const ROOK_ATTACK_OFFSET: [Bitboard; 64] = {:?};\n\n", offset).as_str());
 
     let (lut, offset) = create_king_attack_lut();
-    buf.push_str(format!("pub const KING_ATTACK_LUT: [Bitboard; {}] = {:#018X?};\n", lut.len(), lut).as_str());
-    buf.push_str(format!("pub const KING_ATTACK_OFFSET: [Bitboard; 64] = {:?};\n", offset).as_str());
+    buf.push_str(format!("pub const KING_ATTACK_LUT: [Bitboard; {}] = [\n", lut.len()).as_str());
+    for s in compress_table(lut) { buf.push_str(&("    ".to_owned() + &s + "\n")) }
+    buf.push_str("];\n\n");
+
+    buf.push_str(format!("pub const KING_ATTACK_OFFSET: [Bitboard; 64] = {:?};\n\n", offset).as_str());
 
     // Write the buffer to the output file, returning either a success value or an error.
     output_file.write_all(buf.as_bytes())
